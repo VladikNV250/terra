b9faac092d53f13da8de77caa36aeb3b32e6d9c4
VladikNV250 <vladiknemichev@gmail.com>
2026-05-14 01:04:28 +0300

    fix: copy wasm_exec to src/worker folder

--- engine/build.sh
+++ engine/build.sh
@@ -1,5 +1,5 @@
 GOOS=js GOARCH=wasm go build -o ../public/main.wasm ./main.go
 
-cp "$(go env GOROOT)/lib/wasm/wasm_exec.js" ../public/
+cp "$(go env GOROOT)/lib/wasm/wasm_exec.js" ../src/worker/
 
 echo "Build finished!"
