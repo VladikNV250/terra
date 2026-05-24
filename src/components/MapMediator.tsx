bd9be9095b0b21285d218f69f75b7d8f2b916e00
VladikNV250 <vladiknemichev@gmail.com>
2026-05-24 23:08:13 +0300

    feat: redesign map control

--- src/components/MapMediator.tsx
+++ src/components/MapMediator.tsx
@@ -1,26 +1,23 @@
-import { Flex, Heading, Separator } from "@radix-ui/themes";
+import { Box } from "@radix-ui/themes";
 import { Map } from "./Map";
 import { MapControl } from "./MapControl";
 import { useMapControl } from "../hooks";
-
-
 
 export const MapMediator = () => {
     const { terrainConfig, updateConfig } = useMapControl();
 
     return (
-        <Flex justify="between" className="w-full h-full p-3">
-            <Flex direction="column" gapY="4" className="bg-(--gray-3) p-4 flex flex-col gap-y-4 rounded-xl">
-                <Heading size={"6"} weight={"bold"} color="gray" align={"center"} highContrast>
-                    Terra
-                </Heading>
-                <Separator size={"4"} />
+        <Box className="w-full h-full relative overflow-hidden">
+            <Box className="absolute inset-0 z-0">
+                <Map terrainConfig={terrainConfig} />
+            </Box>
+            
+            <Box className="absolute top-4 right-4 z-10 bottom-4 pointer-events-none">
                 <MapControl
                     terrainConfig={terrainConfig}
                     updateConfig={updateConfig}
                 />
-            </Flex>
-            <Map terrainConfig={terrainConfig} />
-        </Flex>
+            </Box>
+        </Box>
     );
 };
