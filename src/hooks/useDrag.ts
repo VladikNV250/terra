0af92a0731b3a535a854e7c4c5852bf937d9dd4d
VladikNV250 <vladiknemichev@gmail.com>
2026-05-24 23:10:19 +0300

    feat: change useDrag logic

--- src/hooks/useDrag.ts
+++ src/hooks/useDrag.ts
@@ -5,11 +5,13 @@ import type { Vector2D } from "../types/math";
 
 interface UseDragOptions {
     onDragEnd?: (offset: Vector2D) => void;
+    onDragMove?: (offset: Vector2D) => void;
 }
 
 export const useDrag = (options?: UseDragOptions) => {
     const isDraggingRef = useRef(false);
     const dragStartRef = useRef<Vector2D>({ x: 0, y: 0 });
+    const accumulatedOffsetRef = useRef<Vector2D>({ x: 0, y: 0 });
     const [dragOffset, setDragOffset] = useState<Vector2D>({ x: 0, y: 0 });
 
     const handlePointerDown = (e: PointerEvent<HTMLElement>) => {
@@ -23,14 +25,20 @@ export const useDrag = (options?: UseDragOptions) => {
 
         if (e.buttons !== 1) {
             isDraggingRef.current = false;
-            setDragOffset({ x: 0, y: 0 });
             return;
         }
 
         const distanceX = e.clientX - dragStartRef.current.x;
         const distanceY = e.clientY - dragStartRef.current.y;
 
-        setDragOffset({ x: distanceX, y: distanceY });
+        setDragOffset({ 
+            x: accumulatedOffsetRef.current.x - distanceX, 
+            y: accumulatedOffsetRef.current.y - distanceY 
+        });
+
+        if (options?.onDragMove) {
+            options.onDragMove(dragOffset);
+        }
     };
 
     const handlePointerUp = (e: PointerEvent<HTMLElement>) => {
@@ -38,18 +46,18 @@ export const useDrag = (options?: UseDragOptions) => {
         
         e.currentTarget.releasePointerCapture(e.pointerId);
         isDraggingRef.current = false;
+        
+        accumulatedOffsetRef.current = dragOffset;
 
         if (options?.onDragEnd) {
-            options.onDragEnd({
-                x: e.clientX - dragStartRef.current.x,
-                y: e.clientY - dragStartRef.current.y,
-            });
+            options.onDragEnd(dragOffset);
         }
     };
 
     const handlePointerLeave = handlePointerUp;
 
     const resetDrag = useCallback(() => {
+        accumulatedOffsetRef.current = { x: 0, y: 0 };
         setDragOffset({ x: 0, y: 0 })
     }, [])
 
